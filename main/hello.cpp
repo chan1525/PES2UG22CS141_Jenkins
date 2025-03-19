9e51511eb91fe4fc9ea58d03bf8f506d70790c50
Channaveer Upase <148029766+chan1525@users.noreply.github.com>
2025-03-19 10:00:54 +0530

    initial commit

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,5 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
-  cout << " I have made change here successfully" ;
   return 0;
 }
